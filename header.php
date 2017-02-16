1575900fa3d24e1cf37dd61b8b37f8eb65a15f1c
TorbjornLarsson <torbjorn.b.g.larsson@gmail.com>
2017-02-16 15:29:40 +0100

    Update header.php
    Removed User.

--- header.php
+++ header.php
@@ -9,52 +9,42 @@
     <meta name="author" content="">
     <link rel="icon" href="./favicon.ico">
 
-    <title>Tools</title>
+    <title>Header</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="./bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-  
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
     <!-- Custom styles for this template -->
     <link href="signin.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
 
     <div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Group 3</a>
-			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="./home.php">Home</a></li>
-					<li><a href="./tools.php">Tools</a></li>
-					<li><a href="./user.php">User</a></li>
-					<li><a href="./map.php">Map</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</div>
+        <div class="container">
+                <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Marine</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                                <li><a href="./home.php">Home</a></li>
+                                <li><a href="./compare.php">Tools</a></li>
+                                <li><a href="./insertstationform.php">Admin</a></li>
+                                <li><a href="./map.php">Map</a></li>
+                        </ul>
+                </div><!--/.nav-collapse -->
+        </div>
+    </div>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
