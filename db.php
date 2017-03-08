d6012902117c97871c1301ac7129257c3b9ffaa0
indajuan <indajuan@users.noreply.github.com>
2017-03-08 13:46:09 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 	$hostname = "localhost";
 	$username = "root";
 	$password = "LIMS.2017.Uppsala";
-	$database = "marine";
+	$database = "Marine";
 	$conn = new mysqli($hostname, $username, $password, $database)
 	or die("Could not open connection to database");
 ?>
