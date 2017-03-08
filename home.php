46527e196a5f32e0656c8bc025b90980d2725ef9
indajuan <indajuan@users.noreply.github.com>
2017-03-08 13:55:19 +0100

    Introduction
    Adding a little introduction to home web page

--- home.php
+++ home.php
@@ -17,6 +17,12 @@
             <p class="lead">Group 3 IMS project</p>
           </div>
 
+This LIMS project aims to administrate and present physical oceanographic and biological data from the midriff archipelago region (MAR) of the Gulf of California, Mexico. The Gulf of California is a semi-enclosed sea, highly dynamic and productive. The MAR in the Gulf of California is characterized by an intense tidal mixing producing sharp thermal fronts.
+            
+The physical oceanographic processes are used to describe the distribution of larval fish habitats around the MAR in a three-dimensional form. The physical variables included in this LIMS are temperature, salinity, oxygen and fluorescence. The physical variables came from a SeaBird 911plus conductivity, temperature and depth (CTD) profiler, and were provided by Dr. Emilio A. Inda-Diaz. The biological variables include the species, larval stage and abundance from 0 to 200m depth by 50m stratum, and were provided by Dr. Emilio A. Inda-Diaz. The sampling methods for the physical and biological variables are described in Inda-Diaz et al. (2010).
+
+Inda-Díaz Emilio A., Sánchez-Velasco Laura, Lavín Miguel F. 2010. Journal of Plankton Research 32(9): 1241-1254. 
+            
         </div><!-- /.container -->
 
     </body>
